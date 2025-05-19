e9b704c6bc947ff585b04d6c1b4bed35629afff7
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-05-19 16:25:05 +0530

    xcfdf

---
+++ sample.tsx
@@ -0,0 +1,35 @@
+import { Title, Table } from "../../../components/UI";
+import Syntax from "../../../components/Syntax/Syntax";
+export const FlutterSdk = () => {
+  return (
+    <>
+      {" "}
+      <Syntax
+        language="javascript"
+        code={`import { FlutterSdk } from "./FlutterSdk";`}
+      />{" "}
+      <Title>Flutter sdk</Title>{" "}
+      <Table textAlign="center">
+        {" "}
+        <thead>
+          {" "}
+          <tr>
+            {" "}
+            <th>Column 1</th> <th>Column 2</th> <th>Column 3</th>{" "}
+          </tr>{" "}
+        </thead>{" "}
+        <tbody>
+          {" "}
+          <tr>
+            {" "}
+            <td>Row 1, Col 1</td> <td>Row 1, Col 2</td> <td>Row 1, Col 3</td>{" "}
+          </tr>{" "}
+          <tr>
+            {" "}
+            <td>Row 2, Col 1</td> <td>Row 2, Col 2</td> <td>Row 2, Col 3</td>{" "}
+          </tr>{" "}
+        </tbody>{" "}
+      </Table>{" "}
+    </>
+  );
+};
